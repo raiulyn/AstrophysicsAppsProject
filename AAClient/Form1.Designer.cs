f19ff0f970e50c90c190d0fcff8335a1887964a6
30082866 <30082866@PC-603654>
2024-03-05 14:37:55 +0800

    Added a WIP Client app

---
+++ AAClient/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace AAClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ResultsGridView = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.StarVelocityLabel = new System.Windows.Forms.Label();
+            this.StarDistanceLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.EventHorizenLabel = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.EnglishButton = new System.Windows.Forms.Button();
+            this.FrenchButton = new System.Windows.Forms.Button();
+            this.GermanButton = new System.Windows.Forms.Button();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ColorPickerButton = new System.Windows.Forms.Button();
+            this.FontPickerButton = new System.Windows.Forms.Button();
+            this.ThemePickerButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.ResultsGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ResultsGridView
+            // 
+            this.ResultsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultsGridView.Location = new System.Drawing.Point(368, 12);
+            this.ResultsGridView.Name = "ResultsGridView";
+            this.ResultsGridView.Size = new System.Drawing.Size(375, 317);
+            this.ResultsGridView.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // StarVelocityLabel
+            // 
+            this.StarVelocityLabel.AutoSize = true;
+            this.StarVelocityLabel.Location = new System.Drawing.Point(106, 108);
+            this.StarVelocityLabel.Name = "StarVelocityLabel";
+            this.StarVelocityLabel.Size = new System.Drawing.Size(66, 13);
+            this.StarVelocityLabel.TabIndex = 2;
+            this.StarVelocityLabel.Text = "Star Velocity";
+            // 
+            // StarDistanceLabel
+            // 
+            this.StarDistanceLabel.AutoSize = true;
+            this.StarDistanceLabel.Location = new System.Drawing.Point(103, 182);
+            this.StarDistanceLabel.Name = "StarDistanceLabel";
+            this.StarDistanceLabel.Size = new System.Drawing.Size(71, 13);
+            this.StarDistanceLabel.TabIndex = 4;
+            this.StarDistanceLabel.Text = "Star Distance";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(184, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Location = new System.Drawing.Point(106, 265);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(67, 13);
+            this.TemperatureLabel.TabIndex = 6;
+            this.TemperatureLabel.Text = "Temperature";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(46, 281);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(184, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // EventHorizenLabel
+            // 
+            this.EventHorizenLabel.AutoSize = true;
+            this.EventHorizenLabel.Location = new System.Drawing.Point(103, 348);
+            this.EventHorizenLabel.Name = "EventHorizenLabel";
+            this.EventHorizenLabel.Size = new System.Drawing.Size(74, 13);
+            this.EventHorizenLabel.TabIndex = 8;
+            this.EventHorizenLabel.Text = "Event Horizon";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(46, 391);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(148, 20);
+            this.textBox4.TabIndex = 9;
+            // 
+            // EnglishButton
+            // 
+            this.EnglishButton.Location = new System.Drawing.Point(388, 348);
+            this.EnglishButton.Name = "EnglishButton";
+            this.EnglishButton.Size = new System.Drawing.Size(75, 23);
+            this.EnglishButton.TabIndex = 11;
+            this.EnglishButton.Text = "English(UK)";
+            this.EnglishButton.UseVisualStyleBackColor = true;
+            // 
+            // FrenchButton
+            // 
+            this.FrenchButton.Location = new System.Drawing.Point(388, 377);
+            this.FrenchButton.Name = "FrenchButton";
+            this.FrenchButton.Size = new System.Drawing.Size(75, 23);
+            this.FrenchButton.TabIndex = 12;
+            this.FrenchButton.Text = "French";
+            this.FrenchButton.UseVisualStyleBackColor = true;
+            // 
+            // GermanButton
+            // 
+            this.GermanButton.Location = new System.Drawing.Point(388, 406);
+            this.GermanButton.Name = "GermanButton";
+            this.GermanButton.Size = new System.Drawing.Size(75, 23);
+            this.GermanButton.TabIndex = 13;
+            this.GermanButton.Text = "German";
+            this.GermanButton.UseVisualStyleBackColor = true;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(228, 51);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(83, 23);
+            this.CalculateButton.TabIndex = 14;
+            this.CalculateButton.Text = "Calculate All";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            // 
+            // ColorPickerButton
+            // 
+            this.ColorPickerButton.Location = new System.Drawing.Point(638, 377);
+            this.ColorPickerButton.Name = "ColorPickerButton";
+            this.ColorPickerButton.Size = new System.Drawing.Size(88, 23);
+            this.ColorPickerButton.TabIndex = 15;
+            this.ColorPickerButton.Text = "Pick Colors";
+            this.ColorPickerButton.UseVisualStyleBackColor = true;
+            // 
+            // FontPickerButton
+            // 
+            this.FontPickerButton.Location = new System.Drawing.Point(638, 406);
+            this.FontPickerButton.Name = "FontPickerButton";
+            this.FontPickerButton.Size = new System.Drawing.Size(88, 23);
+            this.FontPickerButton.TabIndex = 16;
+            this.FontPickerButton.Text = "Pick Fonts";
+            this.FontPickerButton.UseVisualStyleBackColor = true;
+            // 
+            // ThemePickerButton
+            // 
+            this.ThemePickerButton.Location = new System.Drawing.Point(638, 348);
+            this.ThemePickerButton.Name = "ThemePickerButton";
+            this.ThemePickerButton.Size = new System.Drawing.Size(88, 23);
+            this.ThemePickerButton.TabIndex = 17;
+            this.ThemePickerButton.Text = "Pick Themes";
+            this.ThemePickerButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(236, 198);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Calculate";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(236, 278);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Calculate";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(236, 375);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 21;
+            this.button4.Text = "Calculate";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 394);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "X 10";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(46, 366);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(184, 20);
+            this.numericUpDown1.TabIndex = 23;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ThemePickerButton);
+            this.Controls.Add(this.FontPickerButton);
+            this.Controls.Add(this.ColorPickerButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.GermanButton);
+            this.Controls.Add(this.FrenchButton);
+            this.Controls.Add(this.EnglishButton);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.EventHorizenLabel);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.StarDistanceLabel);
+            this.Controls.Add(this.StarVelocityLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ResultsGridView);
+            this.Name = "Form1";
+            this.Text = "AstrophysicsApps Client";
+            ((System.ComponentModel.ISupportInitialize)(this.ResultsGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView ResultsGridView;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label StarVelocityLabel;
+        private System.Windows.Forms.Label StarDistanceLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label EventHorizenLabel;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button EnglishButton;
+        private System.Windows.Forms.Button FrenchButton;
+        private System.Windows.Forms.Button GermanButton;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ColorPickerButton;
+        private System.Windows.Forms.Button FontPickerButton;
+        private System.Windows.Forms.Button ThemePickerButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ AAClient/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AAClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            SetupMenus();
+
+            ResultsGridView.ColumnCount = 5;
+            ResultsGridView.Columns[0].Name = "Release Date";
+            ResultsGridView.Columns[1].Name = "Track";
+            ResultsGridView.Columns[2].Name = "Title";
+            ResultsGridView.Columns[3].Name = "Artist";
+            ResultsGridView.Columns[4].Name = "Album";
+            PopulateDataGridView(ResultsGridView);
+
+        }
+
+        private void SetupMenus()
+        {
+            this.Menu = new MainMenu();
+            MenuItem item = new MenuItem("File");
+            this.Menu.MenuItems.Add(item);
+            item.MenuItems.Add("Save", new EventHandler(Save_Click));
+            item.MenuItems.Add("Open", new EventHandler(Open_Click));
+            item = new MenuItem("Edit");
+            this.Menu.MenuItems.Add(item);
+            item.MenuItems.Add("Copy", new EventHandler(Copy_Click));
+            item.MenuItems.Add("Paste", new EventHandler(Paste_Click));
+        }
+        private void Paste_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Copy_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PopulateDataGridView(DataGridView view)
+        {
+
+            string[] row0 = { "11/22/1968", "29", "Revolution 9",
+            "Beatles", "The Beatles [White Album]" };
+            string[] row1 = { "1960", "6", "Fools Rush In",
+            "Frank Sinatra", "Nice 'N' Easy" };
+            string[] row2 = { "11/11/1971", "1", "One of These Days",
+            "Pink Floyd", "Meddle" };
+            string[] row3 = { "1988", "7", "Where Is My Mind?",
+            "Pixies", "Surfer Rosa" };
+            string[] row4 = { "5/1981", "9", "Can't Find My Mind",
+            "Cramps", "Psychedelic Jungle" };
+            string[] row5 = { "6/10/2003", "13",
+            "Scatterbrain. (As Dead As Leaves.)",
+            "Radiohead", "Hail to the Thief" };
+            string[] row6 = { "6/30/1992", "3", "Dress", "P J Harvey", "Dry" };
+
+            view.Rows.Add(row0);
+            view.Rows.Add(row1);
+            view.Rows.Add(row2);
+            view.Rows.Add(row3);
+            view.Rows.Add(row4);
+            view.Rows.Add(row5);
+            view.Rows.Add(row6);
+        }
+    }
+}
